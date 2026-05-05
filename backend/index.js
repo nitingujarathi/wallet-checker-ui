45206d15362452e185ea8cf0dc72e0afa984415e
Barrel <124461831+nitingujarathi@users.noreply.github.com>
2026-05-05 16:15:30 +0000

    improve server log

--- backend/index.js
+++ backend/index.js
@@ -11,5 +11,4 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`✅ Backend listening at http://localhost:${PORT}`);
-});
+console.log(`🚀 Server running on port ${PORT}`);});
